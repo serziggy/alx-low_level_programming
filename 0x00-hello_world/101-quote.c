402dd6e677c378111667c2989e7abcb0c206056e
serziggy <derrickfalls728@gmail.com>
2023-07-14 04:30:58 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,9 @@
 /**
  * main - A c program that prints a line to the standard error
  *
- * Return 1 (Success)
+ * Return: 1
  */
+
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
