bf75fd19d0091ebf5808c8b0caface2c36a56e41
serziggy <derrickfalls728@gmail.com>
2023-07-14 03:30:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
- * return 1 (success)
+ *
+ * Return 1 (Success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
 return (1);
 }
