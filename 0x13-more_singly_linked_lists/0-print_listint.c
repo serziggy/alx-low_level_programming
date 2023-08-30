903faf5ea7a2b327ff7205564a0a81d3978943f3
serziggy <derrickfalls728@gmail.com>
2023-08-30 13:22:35 -0700

    1st structure

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,23 +1,19 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: list's name
- * Return: number of nodes
+ * print_listint - prints all the elements of a listint_t list
+ * @h: head
+ * Return: the number of nodes
  */
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t node = 0;
 
 	while (h)
 	{
-		if (h->str == 0)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		i++;
+		printf("%d\n", h->n);
 		h = h->next;
-
+		node++;
 	}
-	return (i);
+	return (node);
 }
